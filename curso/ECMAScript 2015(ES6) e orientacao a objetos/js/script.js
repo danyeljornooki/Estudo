8b5d2767811f871c1b31ec6b5424e758841d59de
danyeljornooki <danyeljornookio@gmail.com>
2024-11-24 02:18:34 -0300

    mais conteudo

--- curso/ECMAScript 2015(ES6) e orientacao a objetos/js/script.js
+++ curso/ECMAScript 2015(ES6) e orientacao a objetos/js/script.js
@@ -150,6 +150,8 @@ Calculadora.calcular()
 
 
 // ------OO Pilar da Abstração-------
+// Entidade, Identidade, Caracteristicas, ações
+
 // modelo da objeto
 class ContaBancaria {
     // agencia
@@ -203,7 +205,6 @@ console.log(y.consultarSaldo());
 // cadeira
 // let qtde_pernas = 4
 // let giratoria = false
-*/
 // let cor = 'azul'
 
 // Array de cadeiras
@@ -245,18 +246,364 @@ console.log(cadeiras);
 
 gira_cadeira(0)
 
-// // Paradigma de OO
-// class Cadeira {
-//     constructor(qtde_pernas, giratoria, cor) {
-//         this.qtde_pernas = qtde_pernas
-//         this.giratoria = giratoria
-//         this.cor = cor
-//     }
+// Paradigma de OO
+class Cadeira {
+    constructor(qtde_pernas, giratoria, cor) {
+        this.qtde_pernas = qtde_pernas
+        this.giratoria = giratoria
+        this.cor = cor
+    }
 
-//     gira_cadeira() {
-//         this.giratoria === true ? 'Girou' : 'Cadeira não é giratoria'
+    giraCadeira() {
+        if (this.giratoria === true) {
+            console.log('Girou');
+        } else{
+            console.log('Cadeira não é giratoria');
+        }
+    }
+}
+
+// let cadeira = new Cadeira(4, false, 'azul')
+// let cadeira2 = new Cadeira(1, true, 'vermelha')
+
+// cadeira.giraCadeira()
+// cadeira2.giraCadeira()
+
+let cadeiras = []
+
+cadeiras.push(new Cadeira(4, false, 'azul'))
+cadeiras.push(new Cadeira(1, true, 'vermelha'))
+
+console.log(cadeiras);
+
+
+class Sofa {
+    constructor(cor, qtde_lugares, reclinavel) {
+        this.cor = cor
+        this.qtde_lugares = qtde_lugares
+        this.reclinavel = reclinavel
+    }
+
+    reclinarSofa(){
+        if (this.reclinavel === true) {
+            console.log('cor sofá: ' + this.cor + ', Esse sofá foi reclinado');
+        } else{
+            console.log('cor sofá: ' + this.cor + ', Esse sofá não é reclinavel')
+        }
+    }
+}
+
+let sofas = []
+sofas.push(new Sofa('vermelha', 3, false))
+sofas.push(new Sofa('marrom', 4, true))
+sofas.push(new Sofa('verde', 2, true))
+
+for (let index = 0; index < sofas.length; index++) {
+    sofas[index].reclinarSofa()
+}
+
+
+// ----------- OO Pilar do Encapsulamento----------
+// Seguro, Reutilizável
+
+class Tv {
+    constructor() {
+        this._relacaoCanais = Array(2, 4, 5, 7, 10)
+        this._canalAtivo = 5
+        this._volume = 20
+    }
+    // getters setters
+    get canalAtivo(){
+        return this._canalAtivo
+    }
+
+    set canalAtivo(canal){
+        // 
+        if (this._relacaoCanais.indexOf(canal) !== -1){
+            this._canalAtivo = canal
+        }         
+    }
+}
+
+let tv = new Tv()
+
+tv.canalAtivo = 7
+console.log(tv.canalAtivo);
+
+// -------OO Pilar Herança-----
+// Reutilizável, Manutenção
+
+class Animal {
+    constructor() {
+        this.cor = ''
+        this.tamanho = null
+        this.peso = null
+    }
+
+    dormir(){
+        console.log('dormir');
+        
+    }
+}
+
+class Cachorro extends Animal{
+    constructor() {
+        super()
+        this.orelha = 'Grandes e caídas'
+    }
+
+    correr() {
+        console.log('correr');
+    }
+
+    rosnar() {
+        console.log('rosnar');
+    }
+}
+
+class Passaro extends Animal{
+    constructor() {
+        super()
+        this.bico = 'Curto'
+    }
+
+    voar() {
+        console.log('Voar');
+    }
+
+}
+
+class Papagaio extends Passaro{
+    constructor() {
+        super()
+        this.bico = 'Médio'
+        this.sabeFalar = true
+    }
+
+    falar(){
+        console.log('Sabe falar');
+    }
+}
+
+let cachorro = new Cachorro()
+let passaro = new Passaro()
+let papagaio = new Papagaio()
+
+console.log(cachorro);
+console.log(passaro);
+console.log(papagaio);
+
+
+
+class Animal {
+    constructor(cor, tamanho, peso) {
+        this.cor = cor
+        this.tamanho = tamanho
+        this.peso = peso
+    }
+
+    dormir() {
+        console.log('dormir');
+
+    }
+}
+class Passaro extends Animal {
+    constructor(bico, cor, tamanho, peso) {
+        super(cor, tamanho, peso)
+        this.bico = bico
+    }
+
+    voar() {
+        console.log('Voar');
+    }
+
+}
+
+class Papagaio extends Passaro {
+    constructor(sabeFalar, cor, tamanho, peso) {
+        super('Médio', cor, tamanho, peso)
+        this.sabeFalar = sabeFalar
+    }
+
+    falar() {
+        console.log('Sabe falar');
+    }
+}
+
+// let papagaio = new Papagaio(true, 'verde', 24, 322)
+// console.log(papagaio);
+
+// let papagaio2 = new Papagaio(false, 'azul', 13, 150)
+// console.log(papagaio2);
+
+// // ----teste-----
+// class Quadro {
+//     constructor(altura, largura) {
+//         this.altura = altura
+//         this.largura = largura
 //     }
 // }
 
-// let x = new Cadeira()
-// console.log(x.gira_cadeira(4, false, 'verde'));
+// class QuadroAnimal extends Quadro {
+//     constructor(animal, cor, largura, altura) {
+//         super(altura, largura)
+//         this.animal = animal
+//         this.cor = cor
+//     }
+// }
+
+// // let zebra = new QuadroAnimal('zebra', 'branca', 200, 200)
+// // console.log(`quadro do animal: ${zebra.animal} 
+// //             Cor: ${zebra.cor} 
+// //             tamanho ${zebra.altura}x${zebra.largura}`);
+
+// function montarQuadro() {
+//     let zebra = new QuadroAnimal('zebra', 'branca', 200, 200)
+//     document.write(`quadro do animal: ${zebra.animal} 
+//             Cor: ${zebra.cor} 
+//             tamanho ${zebra.altura}x${zebra.largura}`);
+// }
+
+
+// ------OO Pilar Polimorfismo-----
+// Reutilizável, Sobreescrita de metodos
+
+
+
+class Animal {
+    constructor(cor, tamanho, peso) {
+        this.cor = cor
+        this.tamanho = tamanho
+        this.peso = peso
+    }
+
+    dormir() {
+        console.log('dormir');
+
+    }
+}
+class Passaro extends Animal {
+    constructor(bico, cor, tamanho, peso) {
+        super(cor, tamanho, peso)
+        this.bico = bico
+    }
+
+    voar() {
+        console.log('Voar');
+    }
+
+}
+
+class Papagaio extends Passaro {
+    constructor(sabeFalar, cor, tamanho, peso) {
+        super('Médio', cor, tamanho, peso)
+        this.sabeFalar = sabeFalar
+    }
+
+    falar() {
+        console.log('Sabe falar');
+    }
+}
+
+class Avestruz extends Passaro{
+    constructor(){
+        super('Grande', 'Branco e preto', 250, 15000)
+    }
+
+    enterrarCabeca(){
+        console.log('Enterrar a cabeça');
+        
+    }
+
+    voar(){
+        console.log('Avestruz não voa doido');
+        
+    }
+}
+
+let papagaio = new Papagaio(true, 'verde', 24, 322)
+console.log(papagaio);
+papagaio.voar()
+
+let avestruz = new Avestruz()
+console.log(avestruz)
+avestruz.voar()
+
+// -----Objetos literais / estáticos
+// ---Obejto literal----
+class Produto{
+    constructor(descricao, preco){
+        this.descricao = descricao
+        this.preco = preco
+    }
+
+    verDescricao(){
+        console.log(`${this.descricao} por apenas R$${this.preco}`);
+    }
+}
+
+let produto = new Produto('notbook', 2200)
+produto.verDescricao()
+
+// --------------------------------------------------
+
+let produtoLiteral = {
+    descricao: 'Geladeira',
+    preco: 3000,
+    verDescricao: function(){
+        console.log(`${this.descricao} por apenas R$${this.preco}`);
+    }
+}
+
+produtoLiteral.verDescricao()
+
+// --------------------------
+//  formulário -> servidor
+//  recupera dados e monta um objeto literal
+//  obj literal -> JSON -> HTTP -> Server -> armazena
+
+// --------------------------------
+// -------Melhoria de notação------
+
+let nome = 'jorge' //document.getElementById('')
+let idade = 22
+let sexo = 'masculino'
+let profissao = 'programador'
+
+let objeto = {
+    nome: nome,
+    idade: idade,
+    sexo: sexo,
+    profissao: profissao,
+    exibirResumo: function () {
+        console.log(`Nome: ${this.nome} idade: ${this.idade} sexo: ${this.sexo} profissão: ${this.profissao}`);
+
+    }
+}
+
+objeto.exibirResumo()
+// ----------------------------
+let objeto2 = {
+    nome, //onome = nome variavel / valor = valor da variavel
+    idade,
+    sexo,
+    profissao,
+    exibirResumo() {
+        console.log(`Nome: ${this.nome} idade: ${this.idade} sexo: ${this.sexo} profissão: ${this.profissao}`);
+
+    }
+}
+objeto2.exibirResumo()
+*/
+
+let pessoa = {
+    nome: 'Jose',
+    idade: 34
+}
+
+console.log(pessoa.nome)
+
+pessoa.nome = 'fernanda'
+pessoa.idade = 23
+
+console.log(pessoa.nome, pessoa.idade)
